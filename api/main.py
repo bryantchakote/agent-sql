5f94a4b1545d3d83ecb90b04d572f939d9e4f952
Bryan Tchakote <tchakomanuel@gmail.com>
2025-03-20 16:01:01 +0100

    Changed agent output format to json

--- api/main.py
+++ api/main.py
@@ -1,15 +1,8 @@
 from dotenv import load_dotenv
-
 from fastapi import FastAPI, Form
-
-from pydantic import BaseModel, Field
-
 from langchain import hub as prompts
-
 from langchain_openai import ChatOpenAI
-
 from langgraph.prebuilt import create_react_agent
-
 from langchain_community.utilities import SQLDatabase
 from langchain_community.agent_toolkits import SQLDatabaseToolkit
 
@@ -24,20 +17,10 @@ app = FastAPI()
 db = SQLDatabase.from_uri("sqlite:///db/Chinook.db")
 
 
-# Agent
-class SubmitFinalAnswer(BaseModel):
-    """Formats the final agent response. Returns empty strings fields if necessary."""
-
-    query: str = Field(
-        description="The SQL query that may have been executed to obtain the answer (if applicable)."
-    )
-    result: str = Field(description="The result of the SQL query (if applicable).")
-    answer: str = Field(description="The final answer to the user.")
-
-
+# Initialisation de l'agent
 llm = ChatOpenAI(model="gpt-4o-mini")
-tools = SQLDatabaseToolkit(db=db, llm=llm).get_tools() + [SubmitFinalAnswer]
-prompt = prompts.pull("agent-sql:93819056").format(dialect=db.dialect, top_k=5)
+tools = SQLDatabaseToolkit(db=db, llm=llm).get_tools()
+prompt = prompts.pull("agent-sql").format(dialect=db.dialect, top_k=5)
 agent = create_react_agent(llm, tools, prompt=prompt)
 
 
--- streamlit/app.py
+++ streamlit/app.py
@@ -1,18 +1,17 @@
-# App Streamlit
-import requests
-from langchain_core.messages import AIMessage
 import streamlit as st
+import requests
 import logging
 import json
-import re
 
+
+# Nom de l'application
 st.title("📝 Chatbot SQL Interactif")
 
 # Initialisation de l'historique des messages
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = []
 
-# Prompt utilisateur
+# Question de l'utilisateur
 question = st.chat_input("Posez une question sur la base de données...")
 
 if question:
@@ -23,30 +22,37 @@ if question:
         )
 
         if response.status_code == 200:
-            # Récupération du résultat (avant-dernier message)
-            content = response.json().get("answer")["messages"][-2]["content"]
+            # Récupération du résultat
+            content = (
+                response.json().get("answer")["messages"][-1]["content"].strip("`|json")
+            )
         else:
-            st.error("Erreur lors de l'exécution de la requête.")
             logging.error(
-                f"Erreur lors de l'exécution de la requête. {response.status_code}"
+                f"Erreur lors de l'exécution de la requête. Statut : {response.status_code}"
             )
 
-        # Conversion en dictionnaire Python
-        logging.basicConfig(level=logging.ERROR)
+        # Initialisation des composants de la réponse
+        # Ce seront les valeurs finales si l'agent ne renvoie pas les données au bon format
+        query = result = ""
+        answer = content
 
-        # Extraction des valeurs
-        pattern = r"query='(.*?)'\s+result='(.*?)'\s+answer='(.*?)'"
-        match = re.search(pattern, content)
+        try:
+            # Extraction des valeurs
+            content = json.loads(content)
 
-        if match:
-            query = match.group(1)
-            result = match.group(2)
-            answer = match.group(3)
-        else:
-            # Si l'agent ne renvoie pas les données au bon format, on récupère juste sa réponse finale
-            query = ""
-            result = ""
-            answer = response.json().get("answer")["messages"][-1]["content"]
+            query = content["query"]
+            result = content["result"]
+            answer = content["answer"]
+
+        except json.JSONDecodeError as e:
+            logging.error(f"Erreur de décodage JSON : {e}")
+
+        except KeyError as e:
+            logging.error(f"Erreur de clé JSON : {e}")
+
+        except Exception as e:
+            logging.error(f"Erreur inattendue : {e}")
+            raise
 
         # Mise à jour de l'historique
         st.session_state.chat_history.append(
@@ -58,8 +64,10 @@ for chat in st.session_state.chat_history:
     st.chat_message("user").write(chat["question"])
     st.chat_message("assistant").write(chat["answer"])
 
-    # Toggles pour les détails (requête et résultats éventuels)
-    with st.expander("🔍 Voir la requête exécutée"):
-        st.code(chat["query"], language="sql")
-    with st.expander("📊 Voir le résultat de la requête"):
-        st.code(chat["result"])
+# Toggles pour les détails (requête et résultats éventuels)
+if st.session_state.chat_history:
+    if st.session_state.chat_history[-1]["query"]:
+        with st.expander("🔍 Requête"):
+            st.code(chat["query"], language="sql")
+        with st.expander("📊 Résultat"):
+            st.code(chat["result"], language="txt")
